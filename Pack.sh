f37c4710aeb21cd46d0dd28a62e329ac16122f7b
TimeRainStarSky <Time.Rain.Star.Sky@Gmail.com>
2021-11-19 06:22:19 +0800

    更新 新版dialog 界面

--- Pack.sh
+++ Pack.sh
@@ -1,22 +1,25 @@
 shc -vf "$1"
 echo -n '#脚本已加密，需要源代码请联系作者：时雨丶星空
-test "$(id -u)" -ne 0&&exec su -c sh "$0"
-test "$(id -u)" -ne 0&&echo "\e[1;31m! 获取root权限失败\e[m"&&exit
+[ "$(id -u)" -ne 0 ]&&exec su -c sh "$0"
+[ "$(id -u)" -ne 0 ]&&echo "\e[1;31m! 获取root权限失败\e[m"&&exit
 [ "$0" != "${0%/*}" ]&&cd "${0%/*}"
-EXEC="/data/adb/'"${2##*/}"'.x"
+DIR="/data/adb"
+EXEC="$DIR/'"${2##*/}"'.x"
 MD5="'"$(md5sum "$1.x"|head -c 32)"'"
 BASE64="'"$(cat "$1.x"|base64)"'"
+DIALOG="'"$(cat "$(where dialog)"|base64)"'"
 output_file(){
 echo "\e[1;33m- 正在释放文件\e[m"
 echo "$BASE64"|base64 -d>"$EXEC"
-chmod 755 "$EXEC"
+echo "$DIALOG"|base64 -d>"$DIR/dialog"
+chmod 755 "$EXEC" "$DIR/dialog"
+[ -z "$($DIR/dialog --version 2> /dev/null)" ]&& rm -rf "$DIR/dialog"
 }
 check_exec(){
 if [ -x "$EXEC" ]&&[ "$(md5sum "$EXEC"|head -c 32)" == "$MD5" ];then
   exec "$EXEC" "$(pwd)" "${0##*/}"
 else
   output_file
-  sleep 1
   check_exec
 fi
 }
