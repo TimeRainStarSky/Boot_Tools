cebf9fdc3d416b79fc58bda5d184acb03f624144
TimeRainStarSky <Time.Rain.Star.Sky@Gmail.com>
2021-11-27 20:48:09 +0800

    添加MT管理器和Termux自带curl检测

--- Pack.sh
+++ Pack.sh
@@ -1,7 +1,25 @@
+#!/system/bin/sh
 [ "$(id -u)" -ne 0 ]&&exec su -c sh "$0" "$1" "$2"
-[ "$(id -u)" -ne 0 ]&&echo "\e[1;31m! 获取root权限失败\e[m"&&exit
+R="\e[1;31m"
+G="\e[1;32m"
+Y="\e[1;33m"
+C="\e[1;36m"
+B="\e[1;m"
+O="\e[m"
+[ "$(id -u)" -ne 0 ]&&echo "$R! 获取root权限失败$O"&&exit
+echo "$B************************
+$R HGcc$G Script$Y Pack$C Tools$O
+************************
+    $G酷安：$C时雨丶星空$O
+"
+[ ! -f "$1" ]&&echo "$R! $1文件不存在$O"&&exit
+[ -z "$(which HGcc 2>/dev/null)" ]&&echo "$R! HGcc编译器不存在$O"&&exit
+echo "$Y- 正在使用HGcc编译脚本：$O$1
+"
 HGcc -r "$1" "$1.HGcc"
-echo -n '#脚本已加密，需要源代码请联系作者：时雨丶星空
+echo "
+$Y- 正在打包为自释放脚本：$O$2"
+echo -n '#脚本已经过编译，需要源代码请联系作者：时雨丶星空
 [ "$(id -u)" -ne 0 ]&&exec su -c sh "$0"
 [ "$(id -u)" -ne 0 ]&&echo "\e[1;31m! 获取root权限失败\e[m"&&exit
 [ "$0" != "${0%/*}" ]&&cd "${0%/*}"
@@ -25,3 +43,5 @@ check_exec'>"$2"
 echo -n "version=$(cat "$1"|sed -n s/^VERSION=//p)
 name=$(cat "$1"|sed -n s/^NAME=//p)
 md5=$(md5sum "$2"|head -c 32)">"${2%/*}/version"
+echo "
+$G- 打包完成$O"
