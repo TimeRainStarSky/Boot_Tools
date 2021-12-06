8ed97f2763cf6f5ce265cd5aab166c5d365af138
TimeRainStarSky <Time.Rain.Star.Sky@Gmail.com>
2021-12-06 21:17:11 +0800

    优化自动获取root权限命令

--- Pack.sh
+++ Pack.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
-[ "$(id -u)" -ne 0 ]&&exec su -c sh "$0" "$1" "$2"
 R="\e[1;31m"
 G="\e[1;32m"
 Y="\e[1;33m"
 C="\e[1;36m"
 B="\e[1;m"
 O="\e[m"
+[ "$(id -u)" -ne 0 ]&&echo "$Y- 正在获取root权限$O"&&exec su -c sh "$0" "$@"
 [ "$(id -u)" -ne 0 ]&&echo "$R! 获取root权限失败$O"&&exit
 echo "$B************************
 $R HGcc$G Script$Y Pack$C Tools$O
@@ -21,14 +21,14 @@ echo "
 $Y- 正在打包为自释放脚本$O
 "
 echo -n '#脚本已经过编译，需要源代码请联系作者：时雨丶星空
-[ "$(id -u)" -ne 0 ]&&exec su -c sh "$0"
-[ "$(id -u)" -ne 0 ]&&echo "\e[1;31m! 获取root权限失败\e[m"&&exit
+[ "$(id -u)" -ne 0 ]&&echo "'"$Y"'- 正在获取root权限'"$O"'"&&exec su -c sh "$0" "$@"
+[ "$(id -u)" -ne 0 ]&&echo "'"$R"'! 获取root权限失败'"$O"'"&&exit
 [ "$0" != "${0%/*}" ]&&cd "${0%/*}"
 EXEC="/data/adb/'"${2##*/}"'.HGcc"
 MD5="'"$(md5sum "$1.HGcc"|head -c 32)"'"
 BASE64="'"$(cat "$1.HGcc"|base64)"'"
 output_file(){
-echo "\e[1;33m- 正在释放文件\e[m"
+echo "'"$Y"'- 正在释放文件'"$O"'"
 echo "$BASE64"|base64 -d>"$EXEC"
 chmod 755 "$EXEC"
 }
