df1e47f336b48cd70fafb9ef0cdd5b10f79f0f28
TimeRainStarSky <Time.Rain.Star.Sky@Gmail.com>
2021-12-21 20:55:47 +0800

    优化部分脚本执行逻辑

--- Pack.sh
+++ Pack.sh
@@ -5,54 +5,58 @@ Y="\e[1;33m"
 C="\e[1;36m"
 B="\e[1;m"
 O="\e[m"
-[ "$(id -u)" -ne 0 ]&&echo -e "$Y- 正在获取root权限$O"&&exec su -c sh "$0" "$@"
-[ "$(id -u)" -ne 0 ]&&echo -e "$R! 获取root权限失败$O"&&exit 1
-PATH=/data/adb/magisk:"$(magisk --path)":/system/bin:"$PATH"
-E="$(busybox &>/dev/null&&echo busybox)"
-$E echo -e "$B************************
+if [ "$(id -u)" != 0 ];then
+  echo -e "$Y- 正在获取root权限$O"&&exec su -c "sh $0 $@"
+  [ "$(id -u)" != 0 ]&&echo -e "$R! 获取root权限失败$O"&&exit 1
+fi
+MAGISKPATH="$(magisk --path)"
+PATH="/data/adb/magisk:$MAGISKPATH:$MAGISKPATH/.magisk/busybox:$PATH"
+echo -e "$B************************
 $R HGcc$G Script$Y Pack$C Tools$O
 ************************
     $G酷安：$C时雨丶星空$O"
-[ ! -f "$1" ]&&$E echo -e "$R! $1文件不存在$O"&&exit 1
-[ -z "$(which HGcc 2>/dev/null)" ]&&$E echo -e "$R! HGcc编译器不存在$O"&&exit 1
-$E echo -e "
+[ ! -f "$1" ]&&echo -e "$R! $1文件不存在$O"&&exit 1
+[ -z "$(which HGcc 2>/dev/null)" ]&&echo -e "$R! HGcc编译器不存在$O"&&exit 1
+echo -e "
 $Y- 正在使用HGcc编译脚本$O
 "
 HGcc -r "$1" "$1.HGcc"
-$E echo -e "
+echo -e "
 $Y- 正在打包为自释放脚本$O
 "
-$E echo -en '#脚本已经过编译，需要源代码请联系作者：时雨丶星空
-[ "$(id -u)" -ne 0 ]&&echo -e "'"$Y"'- 正在获取root权限'"$O"'"&&exec su -c sh "$0" "$@"
-[ "$(id -u)" -ne 0 ]&&echo -e "'"$R"'! 获取root权限失败'"$O"'"&&exit 1
-PATH=/data/adb/magisk:"$(magisk --path)":/system/bin:"$PATH"
-E="$(busybox &>/dev/null&&echo busybox)"
+echo -en '#脚本已经过编译，需要源代码请联系作者：时雨丶星空
+if [ "$(id -u)" != 0 ];then
+  echo -e "'"$Y"'- 正在获取root权限'"$O"'"&&exec su -c "sh $0 $@"
+  [ "$(id -u)" != 0 ]&&echo -e "'"$R"'! 获取root权限失败'"$O"'"&&exit 1
+fi
+MAGISKPATH="$(magisk --path)"
+PATH="/data/adb/magisk:$MAGISKPATH:$MAGISKPATH/.magisk/busybox:$PATH"
 [ "$0" != "${0%/*}" ]&&cd "${0%/*}"
 EXEC="/data/adb/'"${2##*/}"'.HGcc"
-MD5="'"$($E md5sum "$1.HGcc"|$E head -c 32)"'"
-BASE64="'"$($E cat "$1.HGcc"|$E base64)"'"
+MD5="'"$(md5sum "$1.HGcc"|head -c 32)"'"
 abort(){
-  $E echo -e "
+  echo -e "
 '"$R"'! $@'"$O"'";exit 1
 }
 output_file(){
-  $E echo -e "'"$Y"'- 正在释放脚本'"$O"'"
-  $E echo -e "$BASE64"|$E base64 -d>"$EXEC"||abort "脚本释放失败"
-  $E chmod 555 "$EXEC"||abort "脚本权限修改失败"
+  BASE64="'"$(cat "$1.HGcc"|base64)"'"
+  echo -e "'"$Y"'- 正在释放脚本'"$O"'"
+  echo -e "$BASE64"|base64 -d>"$EXEC"||abort "脚本释放失败"
+  chmod 555 "$EXEC"||abort "脚本权限修改失败"
 }
 check_exec(){
-  if [ -x "$EXEC" ]&&[ "$($E md5sum "$EXEC"|$E head -c 32)" = "$MD5" ];then
-    $E echo -e "'"$G"'- 脚本校验成功，开始执行'"$O"'"
+  if [ -x "$EXEC" ]&&[ "$(md5sum "$EXEC"|head -c 32)" = "$MD5" ];then
+    echo -e "'"$G"'- 脚本校验成功，开始执行'"$O"'"
     "$EXEC" "$(pwd)" "${0##*/}"||abort "脚本执行失败"
   else
     output_file;check_exec
   fi
 }
 check_exec'>"$2"
-$E echo -en "version=$($E cat "$1"|$E sed -n s/^VERSION=//p)
-name=$($E cat "$1"|$E sed -n s/^NAME=//p)
-md5=$($E md5sum "$2"|$E head -c 32)">"${2%/*}/version"
-$E cat "${2%/*}/version"
-$E echo -e "
+echo -en "version=$(cat "$1"|sed -n s/^VERSION=//p)
+name=$(cat "$1"|sed -n s/^NAME=//p)
+md5=$(md5sum "$2"|head -c 32)">"${2%/*}/version"
+cat "${2%/*}/version"
+echo -e "
 
 $G- 打包完成$O"
