9ee332b19d43fb37c647b8b267ebaef7148e3813
TimeRainStarSky <Time.Rain.Star.Sky@Gmail.com>
2021-12-23 19:52:47 +0800

    优化获取root逻辑，优化并精简脚本

--- Pack.sh
+++ Pack.sh
@@ -1,22 +1,13 @@
-#!/system/bin/sh
-R="[1;31m"
-G="[1;32m"
-Y="[1;33m"
-C="[1;36m"
-B="[1;m"
-O="[m"
+R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 if [ "$(id -u)" != 0 ];then
-  echo "$Y- 正在获取root权限$O"&&exec su -c "sh $0 $@"
-  [ "$(id -u)" != 0 ]&&echo "$R! 获取root权限失败$O"&&exit 1
+  echo "$Y- 正在获取root权限$O";su -c "sh $0 $@"&&exit||echo "$R! 获取root权限失败$O";exit 1
 fi
-MAGISKPATH="$(magisk --path)"
-PATH="/data/adb/magisk:$MAGISKPATH:$MAGISKPATH/.magisk/busybox:$PATH"
+MAGISKPATH="$(magisk --path)";PATH="/data/adb/magisk:$MAGISKPATH:$MAGISKPATH/.magisk/busybox:$PATH"
 echo "$B************************
 $R HGcc$Y Script$G Pack$C Tools$O
 ************************
     $G酷安：$C时雨丶星空$O"
-[ ! -f "$1" ]&&echo "$R! $1文件不存在$O"&&exit 1
-[ -z "$(which HGcc)" ]&&echo "$R! HGcc编译器不存在$O"&&exit 1
+[ ! -f "$1" ]&&echo "$R! $1文件不存在$O"&&exit 1;[ -z "$(which HGcc)" ]&&echo "$R! HGcc编译器不存在$O"&&exit 1
 echo "
 $Y- 正在使用HGcc编译脚本$O
 "
@@ -26,28 +17,21 @@ $Y- 正在打包为自释放脚本$O
 "
 echo -n '#脚本已经过编译，需要源代码请联系作者：时雨丶星空
 if [ "$(id -u)" != 0 ];then
-  echo "'"$Y"'- 正在获取root权限'"$O"'"&&exec su -c "sh $0 $@"
-  [ "$(id -u)" != 0 ]&&echo "'"$R"'! 获取root权限失败'"$O"'"&&exit 1
+  echo "'"$Y"'- 正在获取root权限'"$O"'";su -c "sh $0 $@"&&exit||echo "'"$R"'! 获取root权限失败'"$O"'";exit 1
 fi
-MAGISKPATH="$(magisk --path)"
-PATH="/data/adb/magisk:$MAGISKPATH:$MAGISKPATH/.magisk/busybox:$PATH"
-[ "$0" != "${0%/*}" ]&&cd "${0%/*}"
-EXEC="/data/adb/'"${2##*/}"'.HGcc"
-MD5="'"$(md5sum "$1.HGcc"|head -c 32)"'"
+MAGISKPATH="$(magisk --path)";PATH="/data/adb/magisk:$MAGISKPATH:$MAGISKPATH/.magisk/busybox:$PATH"
+[ "$0" != "${0%/*}" ]&&cd "${0%/*}";EXEC="/data/adb/'"${2##*/}"'.HGcc";MD5="'"$(md5sum "$1.HGcc"|head -c 32)"'"
 abort(){
   echo "
 '"$R"'! $@'"$O"'";exit 1
 }
 output_file(){
   BASE64="'"$(cat "$1.HGcc"|base64)"'"
-  echo "'"$Y"'- 正在释放脚本'"$O"'"
-  echo "$BASE64"|base64 -d>"$EXEC"||abort "脚本释放失败"
-  chmod 555 "$EXEC"||abort "脚本权限修改失败"
+  echo "'"$Y"'- 正在释放脚本'"$O"'";echo "$BASE64"|base64 -d>"$EXEC"||abort "脚本释放失败";chmod 555 "$EXEC"||abort "脚本权限修改失败"
 }
 check_exec(){
   if [ -x "$EXEC" ]&&[ "$(md5sum "$EXEC"|head -c 32)" = "$MD5" ];then
-    echo "'"$G"'- 脚本校验成功，开始执行'"$O"'"
-    "$EXEC" "$(pwd)" "${0##*/}"||abort "脚本执行失败"
+    echo "'"$G"'- 脚本校验成功，开始执行'"$O"'";"$EXEC" "$(pwd)" "${0##*/}"||abort "脚本执行失败"
   else
     output_file;check_exec
   fi
