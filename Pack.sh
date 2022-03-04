f76cb154105044df7933aec0be24181ddc66b991
TimeRainStarSky <Time.Rain.Star.Sky@Gmail.com>
2022-03-04 19:30:53 +0800

    细节优化

--- Pack.sh
+++ Pack.sh
@@ -1,20 +1,13 @@
-R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
-abort(){ echo "
-$R! $@$O";exit 1;}
-echo "$B*************************
+R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m";abort(){ echo "
+$R! $@$O";exit 1;};echo "$B*************************
 $R Shell$Y Script$G Pack$C Tools$O
 *************************
-    ${G}酷安：${C}时雨丶星空$O"
-[ ! -s "$1" ]&&abort "输入文件不存在";[ -z "$2" ]&&abort "输出文件不存在";command -v shc>/dev/null||abort "shc编译器不存在"
-echo "
+    ${G}酷安：${C}时雨丶星空$O";[ -f "$1" ]&&INPUT="$(realpath "$1")"||abort "输入文件不存在";[ -n "$2" ]&&OUTPUT="$(realpath "$2")"||abort "输出文件不存在";command -v shc>/dev/null||abort "shc编译器不存在";echo "
 $Y- 正在使用shc编译脚本$O
-";shc -vrf "$1"||abort "脚本编译失败"
-echo "
+";shc -vrf "$INPUT"||abort "脚本编译失败";echo "
 $Y- 正在打包为自释放脚本$O
 ";echo '#脚本已经过编译，需要源代码请联系作者：时雨丶星空
-if [ "$(id -u)" != 0 ];then echo "'"$Y"'- 正在获取root权限'"$O"'";su -c "sh \"$0\" \"$@\"";exit;fi;PATH="/system/bin:/data/adb/magisk:$(/system/bin/magisk --path)/.magisk/busybox:$PATH";FILE="$(realpath "$0")";EXEC="/data/adb/'"$(basename "$2")"'.x";abort(){ echo "'"$R"'! $@'"$O"'";exit 1;};output_file(){ echo "'"$Y"'- 正在释放脚本'"$O"'";sed "1,2d" "$FILE"|unxz>"$EXEC"||abort "脚本释放失败";chmod 555 "$EXEC"||abort "脚本权限修改失败";OUTPUT=1;};check_exec(){ if [ -x "$EXEC" ]&&[ "$(md5sum "$EXEC"|head -c 32)" = "'"$(md5sum "$1.x"|head -c 32)"'" ];then echo "'"$G"'- 脚本校验成功，开始执行'"$O"'";"$EXEC" "$(dirname "$FILE")" "$FILE"||abort "脚本执行失败";elif [ -z "$OUTPUT" ];then output_file;check_exec;else abort "脚本释放失败";fi;};check_exec;exit'>"$2"||abort "脚本打包失败";xz -9ce "$1.x">>"$2"||abort "脚本打包失败"
-echo -n "$(cat "$1"|sed -n 2p|tr "[A-Z]" "[a-z]"|tr ";" "\n")
-md5=$(md5sum "$2"|head -c 32)">"$(dirname "$2")/version">"$(dirname "$2")/version"||abort "脚本版本文件生成失败"
-cat "$(dirname "$2")/version"||abort "脚本版本文件生成失败";echo "
+if [ "$(id -u)" != 0 ];then echo "'"$Y"'- 正在获取root权限'"$O"'";su -c "sh \"$0\" \"$@\"";exit;fi;PATH="/system/bin:/data/adb/magisk:$(magisk --path)/.magisk/busybox:$PATH";FILE="$(realpath "$0")";EXEC="/data/adb/TimeRainStarSky/'"$(basename "$OUTPUT")"'.x";abort(){ echo "'"$R"'! $@'"$O"'";exit 1;};output_file(){ echo "'"$Y"'- 正在释放脚本'"$O"'";mkdir -p "$(dirname "$EXEC")";sed "1,2d" "$FILE"|unxz>"$EXEC"||abort "脚本释放失败";chmod 555 "$EXEC"||abort "脚本权限修改失败";OUTPUT=1;};check_exec(){ if [ -x "$EXEC" ]&&[ "$(md5sum "$EXEC"|head -c 32)" = "'"$(md5sum "$INPUT.x"|head -c 32)"'" ];then echo "'"$G"'- 脚本校验成功，开始执行'"$O"'";"$EXEC" "$(dirname "$FILE")" "$FILE"||abort "脚本执行失败";elif [ -z "$OUTPUT" ];then output_file;check_exec;else abort "脚本释放失败";fi;};check_exec;exit'>"$OUTPUT"||abort "脚本打包失败";xz -9ce "$INPUT.x">>"$OUTPUT"||abort "脚本打包失败";echo -n "$(cat "$INPUT"|sed -n 2p|tr "[A-Z]" "[a-z]"|tr ";" "\n")
+md5=$(md5sum "$OUTPUT"|head -c 32)">"$(dirname "$OUTPUT")/version"||abort "脚本版本文件生成失败";cat "$(dirname "$OUTPUT")/version"||abort "脚本版本文件生成失败";echo "
 
 $G- 打包完成$O"
